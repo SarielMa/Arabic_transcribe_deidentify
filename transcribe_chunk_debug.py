26d126c710f3ce4540b02b2ee13cab70e4fe089f
sarialMa <rinnkai.ba2019@gmail.com>
2025-12-18 13:06:55 -0500

    start debug transcribe

--- transcribe_chunk_debug.py
+++ transcribe_chunk_debug.py
@@ -8,7 +8,6 @@ if torch.cuda.is_available():
     torch.cuda.reset_peak_memory_stats()
 # -----------------------
 # you need to config the file_path and output_name
-#file_path = r"C:\Users\lm2445\arabic\V8.wav"
 file_path = r"C:\Users\lm2445\arabic\V8.wav"
 output_name = "V8"
 # -----------------------
@@ -103,6 +102,89 @@ if torch.cuda.is_available():
     print(f"Peak GPU memory: {peak / 1024**2:.2f} MB")
 
 
+
+
+# # local arabic audio
+# import torch
+# import gc
+# from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+# import torchaudio
+# import math
+
+# torch.cuda.reset_peak_memory_stats()
+
+# file_path = "/home/lm2445/project_pi_sjf37/lm2445/Arabic/1777.wav"
+# output_name = "1777"
+
+# CHUNK_SECONDS = 30   # no overlap
+# # -----------------------
+# # Load audio
+# # -----------------------
+# waveform, sr = torchaudio.load(file_path)
+
+# if waveform.shape[0] > 1:
+#     waveform = waveform.mean(dim=0)
+
+# waveform = waveform.squeeze()
+
+# device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+# model_id = "lm2445/for_transribing"
+
+# model = AutoModelForSpeechSeq2Seq.from_pretrained(
+#     model_id,
+#     torch_dtype=torch_dtype,
+#     low_cpu_mem_usage=True,
+#     use_safetensors=True
+# ).to(device)
+
+# processor = AutoProcessor.from_pretrained(model_id)
+
+# pipe = pipeline(
+#     "automatic-speech-recognition",
+#     model=model,
+#     tokenizer=processor.tokenizer,
+#     feature_extractor=processor.feature_extractor,
+#     device=device,
+#     torch_dtype=torch_dtype,
+#     return_timestamps=False
+# )
+
+# # -----------------------
+# # Chunk
+# # -----------------------
+# chunk_size = CHUNK_SECONDS * sr
+# final_text = ""
+
+# num_chunks = math.ceil(len(waveform) / chunk_size)
+# print(f"Total chunks: {num_chunks}")
+
+# for i in range(num_chunks):
+#     print(f"Chunk {i+1}/{num_chunks}")
+#     start = i * chunk_size
+#     end = min((i+1) * chunk_size, len(waveform))
+
+#     chunk = waveform[start:end]
+
+#     result = pipe({"array": chunk.numpy(), "sampling_rate": sr})
+#     final_text += " " + result["text"]
+
+#     del result, chunk
+#     gc.collect()
+#     torch.cuda.empty_cache()
+
+# out_file = f"{output_name}_output_transcription.txt"
+
+# with open(out_file, "w", encoding="utf-8") as f:
+#     f.write(final_text.strip())
+
+# print (final_text)
+
+# print(f"\nSaved transcription → {out_file}")
+
+# peak = torch.cuda.max_memory_allocated()
+# print(f"Peak GPU memory: {peak / 1024**2:.2f} MB")
 
 
 
